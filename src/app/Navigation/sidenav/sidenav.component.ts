30e3682877fa196a200837c58b178906c9e7914d
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-07-29 12:20:12 +0530

    update

---
+++ src/app/Navigation/sidenav/sidenav.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.scss']
+})
+export class SidenavComponent implements OnInit {
+
+  @Output() closesidenav = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onclose() {
+    this.closesidenav.emit();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidenavComponent } from './Navigation/sidenav/sidenav.component';
+import { TemplateformComponent } from './dashboard/form/templateform/templateform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidenavComponent,
+    TemplateformComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
